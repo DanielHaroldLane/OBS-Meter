8fb37051d260d38620dbfa6ea8cdd7b3dd3f7f7e
Daniel Lane <dlane@dunelm.com>
2021-07-20 11:26:14 +0100

    Format code

--- serialcomms.js
+++ serialcomms.js
@@ -1,29 +1,29 @@
-const SerialPort = require("serialport");
+const SerialPort = require('serialport')
 
-const serialElement = document.getElementById('serial');
-const modeElement = document.getElementById('mode');
+const serialElement = document.getElementById('serial')
+const modeElement = document.getElementById('mode')
 
-const getSerialPorts = async () => SerialPort.list();
+const getSerialPorts = async () => SerialPort.list()
 
 const modeMap = {
-  0xa0: "Generator",
-  0xa8: "Current A",
-  0xb0: "Current mA",
-  0xc0: "Temperature",
-  0xc8: "Capacitance",
-  0xd0: "Frequency",
-  0xd8: "Diode Mode",
-  0xe0: "Resistance",
-  0xe8: "Millivolts",
-  0xf0: "VCC",
-  0xf8: "VAC",
-};
+  0xa0: 'Generator',
+  0xa8: 'Current A',
+  0xb0: 'Current mA',
+  0xc0: 'Temperature',
+  0xc8: 'Capacitance',
+  0xd0: 'Frequency',
+  0xd8: 'Diode Mode',
+  0xe0: 'Resistance',
+  0xe8: 'Millivolts',
+  0xf0: 'VCC',
+  0xf8: 'VAC',
+}
 
 const signMap = {
-  0x0: "unsigned", // not sure what this is for
-  0x40: "",        // positive sign
-  0x50: "-",       // negative sign
-};
+  0x0: 'unsigned', // not sure what this is for
+  0x40: '', // positive sign
+  0x50: '-', // negative sign
+}
 
 const digitMap = {
   0x30: 0,
@@ -36,11 +36,11 @@ const digitMap = {
   0x37: 7,
   0x38: 8,
   0x39: 9,
-  0x3e: "L",
+  0x3e: 'L',
   0x3f: ' ',
-}; // digits displayed on screen
+} // digits displayed on screen
 
-const parseDigit = (byte) => digitMap[byte];
+const parseDigit = (byte) => digitMap[byte]
 
 const rangeMap = {
   0x0: 1,
@@ -49,75 +49,75 @@ const rangeMap = {
   0x18: 4,
   0x20: 5,
   0x28: 6,
-}; // range digits, used to represent the precision of a number
+} // range digits, used to represent the precision of a number
 
 const getRange = (byte, mode) => {
-  let range = rangeMap[byte & 0x38];
+  let range = rangeMap[byte & 0x38]
 
-  if(mode === 'Resistance') {
-    switch(range) {
+  if (mode === 'Resistance') {
+    switch (range) {
       case 1:
-        range += 2;
-        break;
+        range += 2
+        break
       case 5:
       case 6:
-        range = 1;
-        break;
+        range = 1
+        break
       default:
-        range -= 1;
+        range -= 1
     }
   }
 
-  return range;
+  return range
 }
 
 const decode = (data) => {
-  const mode = modeMap[data[1]];
-  const range = getRange(data[2], mode);
-  const sign = signMap[data[4]];
+  const mode = modeMap[data[1]]
+  const range = getRange(data[2], mode)
+  const sign = signMap[data[4]]
   const value = [
     parseDigit(data[5]),
     parseDigit(data[6]),
     parseDigit(data[7]),
     parseDigit(data[8]),
     parseDigit(data[9]),
-  ];
+  ]
 
   return {
     mode,
     range,
     sign,
-    value: [...value.slice(0, range), ".", ...value.slice(range)].join(""),
-  };
-};
+    value: [...value.slice(0, range), '.', ...value.slice(range)].join(''),
+  }
+}
 
 //[137, 240, 192, 129, 80, 48, 48, 48, 48, 54, 48, 10] // serial data packet is 12 bytes
 
 const pollPorts = async () => {
-  const ports = await getSerialPorts();
-  const selectedPort = ports[0];
-  const buffer = new Buffer.from([0x89]);
+  const ports = await getSerialPorts()
+  const selectedPort = ports[0]
+  const buffer = new Buffer.from([0x89])
 
   const port = new SerialPort(
     selectedPort.path,
     {
       baudRate: 9600,
       dataBits: 8,
-      parity: "none",
+      parity: 'none',
       stopBits: 1,
     },
     false
-  );
+  )
 
-  port.on("data", async (data) => {
-    const dataArr = Array.from(data);
-    const decoded = decode(dataArr);
-    serialElement.innerText = decoded.value;
-    modeElement.innerText = decoded.mode;
-    await port.write(buffer);
-  });
+  port.on('data', async (data) => {
+    const dataArr = Array.from(data)
+    const decoded = decode(dataArr)
+    serialElement.innerText = decoded.value
+    modeElement.innerText = decoded.mode
+    await port.write(buffer)
+  })
 
-  await port.write(buffer);
-};
+  await port.write(buffer)
+}
 
-pollPorts();
+pollPorts()
